6d6d6fb13d0458fb6663e3e506cf71e1407b60b4
Pedro da Luz <pedro.luz@hotmail.com.br>
2021-02-26 03:52:44 -0300

    Form barely working

--- frontend/src/components/FeedbackModal.vue
+++ frontend/src/components/FeedbackModal.vue
@@ -3,18 +3,57 @@ Modal(color="primary", v-model="isOpen", :title="$t('feedback-title')")
   template(v-slot:footer="{ close }")
     v-btn(color="grey", dark, @click="close") {{ $t('close') }}
     v-spacer
-    v-btn(color="success", dark, @click="confirmar") {{ $t('submit') }}
+    v-btn(color="success", dark, @click="submit") {{ $t('submit') }}
+  v-form(ref="form", @submit="submit", :action="action", method="POST")
+    v-container
+      v-row
+        v-col(cols=12)
+          v-text-field(
+            v-model="email",
+            :rules="rules.email",
+            :label="$t('e-mail')",
+            required
+          )
+        v-col(cols=12)
+          v-textarea(
+            v-model="message",
+            :rules="rules.message",
+            :label="$t('message')",
+            required
+          )
 </template>
 
 <i18n>
 {
   "en": {
     "feedback-title": "We <3 feedback",
+    "e-mail": "E-mail",
+    "message": "Mensage",
+    "rules": {
+        "email": {
+            "required": "E-mail is required",
+            "valid": "E-mail must be valid"
+        },
+        "message": {
+            "required": "Message is required"
+        }
+    },
     "close": "Close",
     "submit": "Submit"
   },
   "pt-BR": {
     "feedback-title": "Nós amamos um feedback",
+    "e-mail": "E-mail",
+    "message": "Mensagem",
+    "rules": {
+        "email": {
+            "required": "E-mail obrigatório",
+            "valid": "Insira um e-mail válido"
+        },
+        "message": {
+            "required": "Insira uma mensagem"
+        }
+    },
     "close": "Fechar",
     "submit": "Enviar"
   }
@@ -22,13 +61,14 @@ Modal(color="primary", v-model="isOpen", :title="$t('feedback-title')")
 </i18n>
 
 <script lang="ts">
-import { Vue, Component, Model } from "vue-property-decorator";
+import { Vue, Component, Model, Ref } from "vue-property-decorator";
 import Modal from "./Modal.vue";
+import AbstractService from "../services/AbstractService";
 
 @Component({
   components: {
-    Modal
-  }
+    Modal,
+  },
 })
 export default class FeedbackModal extends Vue {
   @Model("change", { type: Boolean }) readonly value!: boolean;
@@ -37,6 +77,33 @@ export default class FeedbackModal extends Vue {
   }
   set isOpen(value: boolean) {
     this.$emit("change", value);
+  }
+
+  @Ref("form") vForm: Vue;
+
+  action: string = process.env.VUE_APP_FEEDBACKFORM_URL || "/feedback/";
+
+  rules = {
+    email: [
+      (v) => !!v || (this as any).$t("rules.email.required"),
+      (v) => /.+@.+/.test(v) || (this as any).$t("rules.email.valid"),
+    ],
+    message: [(v) => !!v || (this as any).$t("rules.message.required")],
+  };
+
+  email = "";
+  message = "";
+
+  async submit(event?: Event | MouseEvent) {
+    event?.preventDefault();
+    console.log(event);
+    console.log("Valid form: ", (this.vForm as any).validate());
+    AbstractService.post("feedback", {
+      email: this.email,
+      message: this.message,
+    }).then((result) => console.log(result));
+
+    (this.vForm.$el as HTMLFormElement).submit();
   }
 }
 </script>
